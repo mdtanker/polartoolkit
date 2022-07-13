1153a89c01e4a1f4c06bb7cfe8aa47e2966ab227
mdtanker <matt.d.tankersley@gmail.com>
2022-07-13 12:15:24 +1200

    updated config files

--- setup.py
+++ setup.py
@@ -1,30 +1,9 @@
+"""
+Setup script for the Python package.
+Metadata and build configuration are defined in setup.cfg
+Uses setuptools-scm to manage version numbers using git tags.
+"""
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup(
-    name='antarctic_plots',
-    version='0.1',
-    author='Matt Tankersley',
-    author_email='matt.d.tankersley@gmail.com',
-    description='Functions to automate Antarctic data visualization',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/mdtanker/antarctic_plots',
-    project_urls = {
-        "Bug Tracker": "https://github.com/mdtanker/antarctic_plots/issues"
-    },
-    license='MIT',
-    packages=['antarctic_plots'],
-    install_requires=[
-        'pygmt',
-        'pandas',
-        'numpy',
-        'pooch',
-        'xarray',
-        'pyproj',
-        'verde',
-        'rioxarray',
-        ],
-)
+if __name__ == "__main__":
+    setup()
