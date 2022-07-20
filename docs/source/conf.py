f99801ece94087504db2946d0af111d654d5d4e8
mdtanker <matt.d.tankersley@gmail.com>
2022-07-20 14:52:49 +1200

    convert docs to markdown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parcer",
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
