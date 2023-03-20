46f77493c43d3d2deb0b600b42eddc2af1ce04c9
mdtanker <matt.d.tankersley@gmail.com>
2023-03-21 12:38:43 +1300

    add previously excluded tests

--- antarctic_plots/tests/test_fetch.py
+++ antarctic_plots/tests/test_fetch.py
@@ -910,7 +910,6 @@ test = [
 ]
 
 @pytest.mark.fetch
-@pytest.mark.issue
 @pytest.mark.parametrize("test_input,expected", test)
 def test_gia(test_input, expected):
     grid = fetch.gia(test_input)
