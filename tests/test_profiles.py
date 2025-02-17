52db50717353cd5ea0ab0ba62e46bf0df6bd27af
mdtanker <matt.d.tankersley@gmail.com>
2025-02-17 15:59:01 -0500

    chore: add tests to profiles module

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -8,3 +8,76 @@
 """
 Tests for profiles module.
 """
+
+# This is only here to suppress the bug described in
+# https://github.com/pydata/xarray/issues/7259
+# We have to make sure that netcdf4 is imported before
+# numpy is imported for the first time, e.g. also via
+# importing xarray
+import netCDF4  # noqa: F401
+import numpy as np
+import pytest
+
+from polartoolkit import profiles
+
+
+def test_old_profile_module():
+    """
+    Check error raise after import old profile module
+    """
+    with pytest.raises(ImportError) as exception:
+        pass
+    assert "'profile'" in str(exception.value)
+
+
+default_layers_test = [
+    (
+        {
+            "version": "bedmap2",
+            "hemisphere": "south",
+        },
+        [  # mean values of grids
+            602.32306,
+            11.617859,
+            -2127.135,
+        ],
+    ),
+    (
+        {
+            "version": "bedmachine",
+            "hemisphere": "south",
+        },
+        [  # mean values of grids
+            595.5868,
+            7.055721,
+            -2522.419,
+        ],
+    ),
+    (
+        {
+            "version": "bedmachine",
+            "hemisphere": "north",
+        },
+        [  # mean values of grids
+            934.1983,
+            231.09322,
+            -312.9952,
+        ],
+    ),
+]
+
+
+@pytest.mark.parametrize(("test_input", "expected"), default_layers_test)
+def test_default_layers(test_input, expected):
+    """
+    Check default layers
+    """
+    layers = profiles.default_layers(**test_input)
+    names = [v["name"] for v in layers.values()]
+    colors = [v["color"] for v in layers.values()]
+
+    assert names == ["ice", "water", "earth"]
+    assert colors == ["lightskyblue", "darkblue", "lightbrown"]
+    assert [np.nanmean(v["grid"]) for v in layers.values()] == pytest.approx(
+        expected, 0.1
+    )
