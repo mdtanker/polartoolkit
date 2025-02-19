f6fe89fd4e181d6d37f7539430b57e9af6e9ca7d
mdtanker <matt.d.tankersley@gmail.com>
2025-02-19 14:13:42 -0500

    chore: wait to deprecate functions till v1.0.0

--- src/polartoolkit/utils.py
+++ src/polartoolkit/utils.py
@@ -1083,7 +1083,7 @@ def mask_from_shp(
 
 @deprecation.deprecated(
     deprecated_in="0.4.0",
-    removed_in="0.8.0",
+    removed_in="1.0.0",
     current_version=polartoolkit.__version__,
     details="alter_region has been moved to the regions module, use that instead",
 )
